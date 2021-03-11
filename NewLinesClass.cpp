c495e2fc4437db4276a937140265d9a8fa547686
Franck Kamenan <franck.kamenan@yahoo.com>
2021-03-11 17:18:39 +0100

    New Lines, endl manipulator, blank line

---
+++ NewLinesClass.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World! \n";
+	cout << "I am learning C++ \n";
+
+	//New line with the endl manipulator
+	cout << "Who is Lisa Cuddy?" << endl;
+	cout << "She was the dean of medicine. \n";
+
+	//Blank line.
+	cout << "What are Eric Foreman and Robert Chase? \n\n";
+	cout << "They're doctors \n";
+
+	return 0;
+}
