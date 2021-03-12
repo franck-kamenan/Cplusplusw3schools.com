c8c44388c7dc97ddc005eeb515175befc1764a05
Franck Kamenan <franck.kamenan@yahoo.com>
2021-03-12 10:15:58 +0100

    Variables

---
+++ VariablesClass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int myNum = 15;
+	cout << myNum << "\n";
+	int mySecondNum;
+	mySecondNum = 16;
+	cout << mySecondNum << "\n";
+	myNum = 17;
+	cout << myNum << "\n";
+	cout << "\n";
+
+	return 0;
+}
