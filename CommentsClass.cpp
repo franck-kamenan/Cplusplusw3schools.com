056024b14b9856e265bab0f5d10a5233c4a808f1
Franck Kamenan <franck.kamenan@yahoo.com>
2021-03-12 10:02:18 +0100

    Comments

---
+++ CommentsClass.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// This is a comment
+	cout << "Hello World! \n"; // This is a comment
+	/* // This is 
+	a multi-line comment*/
+	cout << "I am learning C++ \n";
+
+	return 0;
+}
