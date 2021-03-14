4c6c8839154e1a1a24cc2875091a3f047aa60581
Franck Kamenan <franck.kamenan@yahoo.com>
2021-03-14 15:47:22 +0100

    While Loop, do/while loop

---
+++ WhileLoopClass.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	while (i < 5)
+	{
+		cout << i << "\n";
+		i++;
+	}	
+	cout << "\n";
+
+	// Do/while loop.
+	int e = 0;
+	do
+	{
+		cout << e << "\n";
+		e++;
+	} 	while (e < 5);
+	cout << "\n";
+	return 0;
+}
